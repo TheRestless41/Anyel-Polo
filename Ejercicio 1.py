28c617fefdb1fb1e59272377df2e19ece4fa78ff
Anyel <anyelpolo6@gmail.com>
2026-05-21 15:37:45 -0500

    ejercicio 1

---
+++ Ejercicio 1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+print("La suma es:", num1 + num2)
+print("La resta es:", num1 - num2)
+print("La multiplicación es:", num1 * num2)
+print("La división es:", num1 / num2)
+
+
+
+
+
+
